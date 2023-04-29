35ad2cd92222097da2537f1952b7ae8aae13f970
Gayrat Vlasov <ggvlasov@gmail.com>
2023-04-29 17:24:01 +0300

    сделал build для index.js

--- build/index.js
+++ build/index.js
@@ -16,7 +16,8 @@ const getIPs = () => {
     const results = {};
     for (const name of Object.keys(nets)) {
         for (const net of nets[name]) {
-            if (net.family === 'IPv4' && !net.internal) {
+            // if (net.family === 'IPv4' && !net.internal) {
+            if (net.family === 'IPv4') {
                 if (!results[name]) {
                     results[name] = [];
                 }
